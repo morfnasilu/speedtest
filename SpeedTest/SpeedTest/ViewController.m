f0e0fed1e027466df4d8ef69c5a0f42015bcc752
Anton <anton.krivchicov@penchat.net>
2018-02-19 09:51:52 +0300

    first commit

---
+++ SpeedTest/SpeedTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SpeedTest
+//
+//  Created by Dmtech on 19.02.18.
+//  Copyright © 2018 Dmtech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
